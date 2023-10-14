66d6081b53b4fce358cdcf8ecf5a933987570bcf
Yumax-panda <yuma0wall@gmail.com>
2023-10-15 04:25:39 +0900

    fix

---
+++ src/app/guild/[guildId]/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import ResultTable from "@/components/Table/ResultTable"
+import Template from "@/components/Template/Template"
+
+export default function GuildPage() {
+  return (
+    <Template titile="test">
+      <ResultTable />
+    </Template>
+  )
+}
