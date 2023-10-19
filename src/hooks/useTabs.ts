12a381edfd1b537d52b37e1db3b2fc9092dc92cc
Yumax-panda <yuma0wall@gmail.com>
2023-10-19 09:54:52 +0900

    fix: deleted unused values

--- src/hooks/useTabs.ts
+++ src/hooks/useTabs.ts
@@ -1,18 +1,11 @@
 import { useState } from "react"
-import type { SyntheticEvent } from "react"
 
 type UseTabsReturn = {
   value: number
   setValue: (value: number) => void
-  handleChange: (event: SyntheticEvent, newValue: number) => void
 }
 
 export const useTabs = (): UseTabsReturn => {
   const [value, setValue] = useState(0)
-
-  const handleChange = (event: SyntheticEvent, newValue: number) => {
-    setValue(newValue)
-  }
-
-  return { value, setValue, handleChange }
+  return { value, setValue }
 }
