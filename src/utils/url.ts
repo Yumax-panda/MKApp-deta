a51c94a77954f78d1f1b4df346bd51df22f2a891
Yumax-panda <yuma0wall@gmail.com>
2023-10-21 14:59:13 +0900

    changed to util func

---
+++ src/utils/url.ts
@@ -0,0 +1,9 @@
+type GuildImageProps = {
+  id: string
+  icon: string | null
+}
+
+export function getGuildImageUrl({ id, icon }: GuildImageProps) {
+  if (!icon) return "./discord.svg"
+  return `https://cdn.discordapp.com/icons/${id}/${icon}.webp`
+}
