57ba5310863ee390827026fd4d507de66881f0d8
Yumax-panda <yuma0wall@gmail.com>
2023-10-22 17:11:36 +0900

    feat: implemented PATCH method to edit results

--- src/app/api/guild/[guildId]/results/route.ts
+++ src/app/api/guild/[guildId]/results/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server"
+import type { Result } from "@/models/result"
 import { DetaClient } from "@/repository/deta"
 
 const client = new DetaClient()
@@ -13,4 +14,11 @@ export async function GET(req: Request, { params }: UrlParams) {
   const { guildId } = params
   const results = await client.result.get(guildId)
   return NextResponse.json(results)
+}
+
+export async function PATCH(req: Request, { params }: UrlParams) {
+  const { guildId } = params
+  const payload = (await req.json()) as Result[]
+  const newResults = await client.result.set(guildId, payload)
+  return NextResponse.json(newResults)
 }
