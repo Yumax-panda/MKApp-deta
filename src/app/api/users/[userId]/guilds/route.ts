cb69ec65bbaa17c06dae7f66de60f492cc4556a9
Yumax-panda <yuma0wall@gmail.com>
2023-10-25 23:38:31 +0900

    automatically use api when no guilds registered

--- src/app/api/users/[userId]/guilds/route.ts
+++ src/app/api/users/[userId]/guilds/route.ts
@@ -10,34 +10,59 @@ type UrlParams = {
   }
 }
 
+type Guild = {
+  id: string
+  name: string
+  icon: string | null
+}
+
+async function fetchGuilds(userId: string): Promise<Guild[]> {
+  const account = await client.account.getAll(userId)
+  if (!account.length) {
+    console.error("Account is not found")
+    return []
+  }
+  const discordAccount = account[0]
+  if (!discordAccount.access_token) {
+    console.error("Access token is not found")
+    return []
+  }
+  const guilds = await fetchPartialGuilds(discordAccount.access_token)
+  return guilds.map((guild) => ({
+    id: guild.id,
+    name: guild.name,
+    icon: guild.icon,
+  }))
+}
+
+async function saveGuilds(userId: string, guilds: Guild[]) {
+  const tasks = guilds.map((guild) => client.guildDetail.upsert(guild))
+  const [updated, _] = await Promise.all([
+    client.guild.put(userId, guilds),
+    ...tasks,
+  ])
+  return updated
+}
+
 export async function GET(req: Request, { params }: UrlParams) {
   const { userId } = params
   const guilds = await client.guild.get(userId)
+  if (!guilds.length) {
+    try {
+      const data = await fetchGuilds(userId)
+      await saveGuilds(userId, data)
+      return NextResponse.json(data)
+    } catch (e) {
+      console.error(e)
+      return NextResponse.json([])
+    }
+  }
   return NextResponse.json(guilds)
 }
 
 export async function PATCH(req: Request, { params }: UrlParams) {
   const { userId } = params
-  const account = await client.account.getAll(userId)
-  if (!account.length) {
-    console.error("Account is not found")
-    return NextResponse.json([])
-  }
-  const discordAccount = account[0]
-  if (!discordAccount.access_token) {
-    console.error("Access token is not found")
-    return NextResponse.json([])
-  }
-  const guilds = await fetchPartialGuilds(discordAccount.access_token)
-  const data = guilds.map((guild) => ({
-    id: guild.id,
-    name: guild.name,
-    icon: guild.icon,
-  }))
-  const tasks = data.map((guild) => client.guildDetail.upsert(guild))
-  const [updated, _] = await Promise.all([
-    client.guild.put(userId, data),
-    ...tasks,
-  ])
-  return NextResponse.json(updated)
+  const guilds = await fetchGuilds(userId)
+  await saveGuilds(userId, guilds)
+  return NextResponse.json(guilds)
 }
