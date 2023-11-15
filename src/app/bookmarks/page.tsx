d67d8fa42e618b0e53ff2426ea4ade34e2545349
Yumax-panda <yuma0wall@gmail.com>
2023-11-16 00:35:44 +0900

    implemented logic to get bookmarks from user

---
+++ src/app/bookmarks/page.tsx
@@ -0,0 +1,29 @@
+import { notFound } from "next/navigation"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { DetaClient } from "@/repository/deta"
+
+async function getBookmarksByUserId(discordId: string) {
+  const client = new DetaClient()
+  return await client.bookmark.get(discordId)
+}
+
+async function getDiscordIdByUserId(userId: string) {
+  const client = new DetaClient()
+  const accounts = await client.account.getAll(userId)
+  if (!accounts.length) return null
+  return accounts[0].providerAccountId
+}
+
+export default async function BookmarksPage() {
+  const session = await getServerSession(authOptions)
+  if (!session) notFound()
+
+  const discordId = await getDiscordIdByUserId(session.user.id)
+
+  if (!discordId) notFound()
+
+  const bookmarks = await getBookmarksByUserId(discordId)
+
+  return <>{JSON.stringify(bookmarks)}</>
+}
