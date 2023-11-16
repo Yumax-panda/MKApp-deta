4d4d54703c6af8916cc3dfe66c039fc4075ae894
Yumax-panda <yuma0wall@gmail.com>
2023-11-16 10:54:11 +0900

    integrated bookmark

--- src/components/Section/Lounge/LoungeSection.tsx
+++ src/components/Section/Lounge/LoungeSection.tsx
@@ -2,18 +2,22 @@ import { Stack } from "@mui/material"
 import LoungeProfile from "./LoungeProfile"
 import MmrHistoryTable from "./MmrHistory/Table"
 import SectionContainer from "@/components/Section/SectionContainer/SectionContainer"
+import BookmarkSelect from "@/components/Select/BookmarkSelect"
 import { type PlayerDetails, getValidMmrChanges } from "@/lib/lounge"
+import type { PinnedPlayer } from "@/repository/bookmark"
 
 type Props = {
   player: PlayerDetails
+  bookmarks: PinnedPlayer[]
 }
 
-function LoungeSection({ player }: Props) {
+function LoungeSection({ player, bookmarks }: Props) {
   const mmrChanges = getValidMmrChanges(player.mmrChanges)
 
   return (
     <SectionContainer>
       <Stack spacing={2}>
+        <BookmarkSelect bookmarks={bookmarks} />
         <LoungeProfile player={player} />
         <MmrHistoryTable changes={mmrChanges} />
       </Stack>
